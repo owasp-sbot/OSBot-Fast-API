9954912264bc3f1d3d65c640b11743a3df39c75d
Dinis Cruz <dinis.cruz@owasp.org>
2023-12-28 14:57:30 +0000

    Added placeholder tests for Integration and Docker

---
+++ tests/docker/test_Docker.py
@@ -0,0 +1,7 @@
+from unittest import TestCase
+
+
+class test_Docker(TestCase):
+
+    def setUp(self):
+        pass                            # docker tests will go here
---
+++ tests/integration/test_Integration.py
@@ -0,0 +1,7 @@
+from unittest import TestCase
+
+
+class test_Integration(TestCase):
+
+    def setUp(self):
+        pass                            # integration tests will go here
