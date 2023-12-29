30273c6f641664d04c2a29a6700c4b4fd55d4c42
Dinis Cruz <dinis.cruz@owasp.org>
2023-12-29 19:56:55 +0000

    started adding code to Http_Shell__Client

--- osbot_fast_api/api/routes/http_shell/Http_Shell__Client.py
+++ osbot_fast_api/api/routes/http_shell/Http_Shell__Client.py
@@ -1,2 +1,75 @@
 class Http_Shell__Client:
-    pass
+    def __init__(self, shell_server_endpoint=None):
+        self.shell_server_endpoint = shell_server_endpoint
+        pass
+
+    def _invoke(self, method_name, method_kwargs=None):
+        event = {'lambda_shell': {'method_name': method_name, 'method_kwargs': method_kwargs}}
+        return Shell_Server().invoke(event)
+
+    def bash(self, command, cwd=None):
+        return self._invoke('bash', {'command': command, 'cwd': cwd})
+
+    def exec(self, executable, params=None, cwd=None):
+        result = self.process_run(executable, params, cwd)
+        std_out = result.get('stdout', '').strip()
+        std_err = result.get('stderr', '').strip()
+        std_console = std_out + std_err
+        if std_console:
+            return std_console
+        if result.get('errorMessage'):
+            return f'Error: {result.get("errorMessage")}'
+        return result
+
+    def exec_function(self, function):
+        return self.python_exec_function(function)
+
+    def process_run(self, executable, params=None, cwd=None):
+        return self._invoke('process_run', {'executable': executable, 'params': params, 'cwd': cwd})
+
+    def reset(self):
+        if self.aws_lambda.s3_bucket is None:  # if these values are not set
+            self.aws_lambda.set_s3_bucket(AWS_Config().lambda_s3_bucket())  # use default values
+            self.aws_lambda.set_s3_key(
+                f'{AWS_Config().lambda_s3_folder_lambdas()}/{self.aws_lambda.original_name}.zip')  # which are needed
+        return self.aws_lambda.update_lambda_code()  # to trigger the update (which will reset the lambda and force a cold start on next lambda invocation)
+
+    def python_exec(self, code):
+        return self._invoke('python_exec', {'code': code})
+
+    def python_exec_function(self, function):
+        function_name = function.__name__
+        function_code = function_source_code(function)
+        exec_code = f"{function_code}\nresult= {function_name}()"
+        return self.python_exec(exec_code)
+
+    # command methods
+
+    def ls(self, path='', cwd='.'):  # todo: fix vuln: this method allows extra process executions via ; and |
+        return self.bash(f'ls {path}', cwd).get('stdout')
+
+    def file_contents(self, path):
+        return self._invoke('file_contents', {'path': path})
+
+    # with not params
+
+    def disk_space(self):
+        return self._invoke('disk_space')
+
+    def list_processes(self):
+        return self._invoke('list_processes')
+
+    def memory_usage(self):
+        return self._invoke('memory_usage')
+
+    def ping(self):
+        return self._invoke('ping')
+
+    def ps(self):
+        return self.exec('ps')
+
+    def pwd(self):
+        return self._invoke('pwd')
+
+    def whoami(self):
+        return self.exec('whoami')
--- tests/unit/api/routes/http_shell/test_Http_Shell__Client.py
+++ tests/unit/api/routes/http_shell/test_Http_Shell__Client.py
@@ -1,9 +1,44 @@
 from unittest import TestCase
 
+from osbot_utils.utils.Dev import pprint
+from pydantic import BaseModel
+
+from osbot_fast_api.api.Fast_API import Fast_API
 from osbot_fast_api.api.routes.http_shell.Http_Shell__Client import Http_Shell__Client
+from osbot_fast_api.api.routes.http_shell.Http_Shell__Server import Http_Shell__Server
+from osbot_fast_api.utils.Fast_API_Server import Fast_API_Server
+
+class Model__Shell_Command(BaseModel):
+    auth_key: str
+    data    : dict
+
 
 
 class test_Http_Shell__Client(TestCase):
+    fast_api        : Fast_API
+    fast_api_server : Fast_API_Server
+    @classmethod
+    def setUpClass(cls) -> None:
+        cls.fast_api = Fast_API()
+        cls.fast_api_server = Fast_API_Server(app=cls.fast_api.app())
+        assert cls.fast_api_server.start() is True
+        cls.fast_api.add_route_post(cls.http_shell_server)
 
-    def setUp(self):
-        self.client = Http_Shell__Client()
+    @classmethod
+    def tearDownClass(cls) -> None:
+        assert cls.fast_api_server.stop() is True
+
+    @staticmethod
+    def http_shell_server(shell_command: Model__Shell_Command):
+        auth_key           = shell_command.auth_key
+        data               = shell_command.data
+        http_shell__server = Http_Shell__Server()
+        return http_shell__server.invoke(data).get('return_value')
+
+
+    def test__fast_api_server(self):
+        data            = {'method_name':'ping', 'method_kwargs': {}}
+        expected_result = 'pong'
+        shell_comamnd = Model__Shell_Command(auth_key='aaa', data=data)
+        assert self.fast_api.routes_paths() == ['/http-shell-server']
+        assert self.fast_api.client().post('/http-shell-server', json=dict(shell_comamnd)).json() == expected_result
