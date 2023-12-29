a40984ae0ef39292737975f5a04001a0e217169e
Dinis Cruz <dinis.cruz@owasp.org>
2023-12-29 17:53:10 +0000

    added header auth to example 2

--- tests/unit/examples/ex_2_with_api_key/test_Fast_API__With_API_Key.py
+++ tests/unit/examples/ex_2_with_api_key/test_Fast_API__With_API_Key.py
@@ -1,9 +1,10 @@
 from unittest import TestCase
 
 from osbot_utils.utils.Dev import pprint
+from osbot_utils.utils.Misc import obj_info
 
 from osbot_fast_api.examples.ex_2_with_api_key.Fast_API__With_API_Key import Fast_API__With_API_Key, \
-    ROUTES_PATHS__WITH_API_KEY
+    ROUTES_PATHS__WITH_API_KEY, EX_2_API_KEY_NAME, EX_2_API_KEY_VALUE
 
 
 class test_Fast_API__With_API_Key(TestCase):
@@ -16,8 +17,20 @@ class test_Fast_API__With_API_Key(TestCase):
     def test_client__an_get_route(self):
         response = self.client.get('/an-get-route')
         assert response.status_code == 200
-        assert response.json()        == 'Hello World'
+        assert response.json()      == 'hello from middleware'
         assert response.headers.get('extra_header') == 'goes here'
+
+    def test_client__secure_data(self):
+        response_1 = self.client.get('/secure-data')
+        assert response_1.status_code                 == 403
+        assert response_1.json()                      == {'detail': 'Not authenticated'}
+        assert response_1.headers.get('extra_header') == 'goes here'
+
+        headers    = {EX_2_API_KEY_NAME : EX_2_API_KEY_VALUE}
+        response_2 = self.client.get(url='/secure-data', headers=headers)
+        assert response_2.status_code                 == 200
+        assert response_2.json()                      == {'message': 'Secure data accessed'}
+        assert response_2.headers.get('extra_header') == 'goes here'
 
     def test_setup_middlewares(self):
         assert self.fast_api.user_middlewares() == [{'function_name': 'an_middleware', 'params': {}, 'type': 'BaseHTTPMiddleware'}]
