61128832174d3a150b0bac61e11398c97e9963b3
Dinis Cruz <dinis.cruz@owasp.org>
2024-08-21 19:57:18 +0100

    refactored in global exception handlers and added option to add default_routes

--- osbot_fast_api/api/Fast_API.py
+++ osbot_fast_api/api/Fast_API.py
@@ -1,19 +1,18 @@
+import traceback
 import types
 
-from fastapi                                                                import FastAPI
+from fastapi                                                                import FastAPI, Request, HTTPException
+from fastapi.exceptions                                                     import RequestValidationError
 from starlette.middleware.wsgi                                              import WSGIMiddleware       # todo replace this with a2wsgi
-
-from osbot_fast_api.api.Fast_API__Http_Events import Fast_API__Http_Events
+from osbot_fast_api.api.Fast_API__Http_Events                               import Fast_API__Http_Events
 from osbot_fast_api.api.middlewares.Middleware__Http_Request                import Middleware__Http_Request
 from osbot_fast_api.api.middlewares.Middleware__Http_Request__Duration      import Middleware__Http_Request__Duration
 from osbot_fast_api.api.middlewares.Middleware__Http_Request__Trace_Calls   import Middleware__Http_Request__Trace_Calls
 from osbot_fast_api.utils.Version                                           import Version
 from osbot_utils.base_classes.Type_Safe                                     import Type_Safe
 from starlette.middleware.cors                                              import CORSMiddleware
-from starlette.responses                                                    import RedirectResponse
+from starlette.responses                                                    import RedirectResponse, JSONResponse
 from starlette.staticfiles                                                  import StaticFiles
-
-from osbot_utils.utils.Dev import pprint
 from osbot_utils.utils.Lists                                                import list_index_by
 from osbot_utils.utils.Misc                                                 import list_set
 from osbot_utils.decorators.lists.index_by                                  import index_by
@@ -22,21 +21,40 @@ from starlette.testclient                                                   impo
 from osbot_fast_api.api.routes.Routes_Config                                import Routes_Config
 from osbot_fast_api.utils.http_shell.Http_Shell__Server                     import Model__Shell_Command, Http_Shell__Server
 from osbot_fast_api.utils.Fast_API_Utils                                    import Fast_API_Utils
-from osbot_utils.utils.Objects import obj_info
 
 DEFAULT_ROUTES_PATHS    = ['/', '/config/status', '/config/version']
 DEFAULT__NAME__FAST_API = 'Fast_API'
 
 class Fast_API(Type_Safe):
-    base_path    : str  = '/'
-    enable_cors  : bool = False
-    name         : str  = None
-    http_events  : Fast_API__Http_Events
+    base_path      : str  = '/'
+    enable_cors    : bool = False
+    default_routes : bool = True
+    name           : str  = None
+    http_events    : Fast_API__Http_Events
 
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
         self.name                      = self.__class__.__name__
         self.http_events.fast_api_name = self.name
+
+    def add_global_exception_handlers(self):      # todo: move to Fast_API
+        app = self.app()
+        @app.exception_handler(Exception)
+        async def global_exception_handler(request: Request, exc: Exception):
+            stack_trace = traceback.format_exc()
+            content = { "detail"      : "An unexpected error occurred." ,
+                        "error"       : str(exc)                        ,
+                        "stack_trace" : stack_trace                     }
+            return JSONResponse( status_code=500, content=content)
+
+        @app.exception_handler(HTTPException)
+        async def http_exception_handler(request: Request, exc: HTTPException):
+            return JSONResponse( status_code=exc.status_code, content={"detail": exc.detail})
+
+        @app.exception_handler(RequestValidationError)
+        async def validation_exception_handler(request: Request, exc: RequestValidationError):
+            return JSONResponse( status_code=400, content={"detail": exc.errors()} )
+
 
     def add_flask_app(self, path, flask_app):
         self.app().mount(path, WSGIMiddleware(flask_app))
@@ -82,10 +100,11 @@ class Fast_API(Type_Safe):
         parent_app.mount(self.base_path, self.app())
 
     def setup(self):
-        self.setup_middlewares        ()        # overwrite to add middlewares
-        self.setup_default_routes     ()
-        self.setup_static_routes      ()
-        self.setup_routes             ()        # overwrite to add routes
+        self.add_global_exception_handlers()
+        self.setup_middlewares            ()        # overwrite to add middlewares
+        self.setup_default_routes         ()
+        self.setup_static_routes          ()
+        self.setup_routes                 ()        # overwrite to add routes
         return self
 
     @index_by
@@ -122,8 +141,9 @@ class Fast_API(Type_Safe):
 
 
     def setup_default_routes(self):
-        self.setup_add_root_route()
-        self.add_routes(Routes_Config)
+        if self.default_routes:
+            self.setup_add_root_route()
+            self.add_routes(Routes_Config)
 
     def setup_add_root_route(self):
         def redirect_to_docs():
